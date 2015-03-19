43cd69aa3d07aa26be36032e9d3c1de8ae3dee90
quimey <quimey.funes@gmail.com>
2015-03-18 22:38:15 -0300

    Agrego metodos a Scissors

--- 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
+++ 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
@@ -1,5 +1,15 @@
 package ar.fiuba.tecnicas.rockpaperscissors;
 
 public class Scissors {
-
+	public Scissors vs(Scissors scissors){
+		return new Scissors();
+	}
+	
+	public Scissors vs(Paper paper){
+		return new Scissors();
+	}
+	
+	public Rock vs(Rock rock){
+		return new Rock();
+	}
 }
